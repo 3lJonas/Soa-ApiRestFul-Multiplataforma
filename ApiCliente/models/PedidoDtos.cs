20f0b84d98c4a168adab0cd90009135f54bc4e65
materubag <mrubio7466@uta.edu.ec>
2025-09-11 23:35:03 -0500

    Agregando DTOs

---
+++ ApiCliente/models/PedidoDtos.cs
@@ -0,0 +1,6 @@
+﻿namespace ApiCliente.models
+{
+    public record PedidoCreateDto(string UsuarioNombre, string Producto, int Cantidad);
+
+    public record PedidoDto(int Id, string Producto, int Cantidad, DateTime FechaCreacion, int UsuarioId, string UsuarioNombre);
+}
