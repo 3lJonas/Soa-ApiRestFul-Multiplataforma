fcac53d10fd463cfadd832379654da3ac575f4ce
El Jonas <jona16lozada@gmail.com>
2025-09-11 22:25:40 -0500

    Se crea el modelo Pedido y el modelo Usuarios

---
+++ ApiPedidos/models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiPedidos.models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string Producto { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+
+        public int UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
---
+++ ApiPedidos/models/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiPedidos.models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+
+        public ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+    }
+}
